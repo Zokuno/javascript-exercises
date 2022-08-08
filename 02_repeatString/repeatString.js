28157ba3c5d2fb6ecf67758ac9e1bb15974c8cd4
Zokuno <joel.brookes@gmail.com>
2022-08-09 09:22:29 +1000

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = string;
+    for (let i = 0; i < num; i++) {
+       var words = word + string;
+    }
 };
 
 // Do not edit below this line
