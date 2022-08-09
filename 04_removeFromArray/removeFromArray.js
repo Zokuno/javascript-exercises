d5b22a3ab47461ed2544273bd277a12acac9f0a4
Zokuno <joel.brookes@gmail.com>
2022-08-09 20:19:32 +1000

    Updated 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, b, c, d, e, f, g, h) {
+    let array = [a, b, c, d];
+    let removeElement = array.splice[e, 1];
+    return removeElement;
 };
 
 // Do not edit below this line
