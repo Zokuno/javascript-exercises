e860090fe28522be713277ad454ec0f436c6f827
Joel <joel.brookes@gmail.com>
2022-08-09 13:07:00 +1000

    Completed 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,10 @@
 const repeatString = function(string, num) {
-    let word = string;
+    let word = '';
+    if (num < 0) return 'ERROR';
     for (let i = 0; i < num; i++) {
-       var words = word + string;
+       word += string;
     }
+    return word;
 };
 
 // Do not edit below this line
