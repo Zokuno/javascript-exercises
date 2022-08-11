7189ec1f4a730498508d1b95b0ba130c650d1d66
Zokuno <joel.brookes@gmail.com>
2022-08-11 22:40:22 +1000

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,22 @@
-const removeFromArray = function(a, b, c, d, e, f, g, h) {
-    let array = [a, b, c, d];
-    let removeElement = array.splice[e, 1];
-    return removeElement;
-};
+const removeFromArray = function (...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
+  
+
+
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
